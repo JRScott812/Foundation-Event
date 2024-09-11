776e75adfba28ea43cf0f197bb104172fc4eb33a
Jake Scott <69864859+TheBiggestOfBoys@users.noreply.github.com>
2024-09-10 20:13:45 -0400

    Fixed time of the event

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-const countDownDate = new Date("Nov 23, 2024 10:00:00").getTime();
+const countDownDate = new Date("Nov 23, 2024 22:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
