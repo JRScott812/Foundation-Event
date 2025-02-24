cb6036905c328ab532c18276720b75fa079e6a27
Jake Scott <69864859+TheBiggestOfBoys@users.noreply.github.com>
2025-02-23 22:42:12 -0500

    Redid Countdown system

--- Countdown.js
+++ Countdown.js
@@ -1,28 +1,33 @@
-// Set the date we're counting down to
-const date = document.getElementById("date");
-const countDownDate = new Date(date).getTime();
+document.addEventListener("DOMContentLoaded", function () {
+    // Set the date we're counting down to
+    const dateElement = document.getElementsByTagName("countdownDate")[0];
+    if (dateElement) {
+        const date = Date.parse(dateElement.innerHTML);
+        const countDownDate = new Date(date).getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function () {
-    // Get today's date and time
-    var now = new Date().getTime();
+        // Update the count down every 1 second
+        var x = setInterval(function () {
+            // Get today's date and time
+            var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = "EXPIRED";
+            }
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Display the result in the element with id="countdown"
+            document.getElementById("countdown").innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
+        }, 1000);
+    } else {
+        console.error('Element with id "date" not found.');
     }
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="countdown"
-    document.getElementById("countdown").innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
-
-    // If the count down is finished, write some text
-}, 1000);
+});
